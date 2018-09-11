057cd18d83d0c81b50bf71f67ea938f5bb0dac0c
Jernej Vivod <je.vivod@gmail.com>
2018-09-11 11:07:05 +0200

    Add instructions for second challenge

---
+++ Challenge_2/challenge_2.R
@@ -0,0 +1,20 @@
+# Implementirajte kalkulator za delo s kompleksnimi števili. Omogoča naj seštevanje, odštevanje, množenje, deljenje in iskanje n-tega primitivnega korena enote.
+# Implement a calculator for working with complex numbers. It should support addition, subtraction, multiplication, division and computing the n-th primitive root of unity.
+
+# Define the following functions:
+
+ # add        // adding complex numbers
+ # multiply   // multiplying complex numbers
+ # multiply   // multiplying complex numbers with real numbers
+ # divide     // division of complex numbers
+ # n_root     // computing the n-th primitive root of unity
+
+# The calculator should support the commands:
+
+# + (addition),
+# - (subtraction),
+# * (multiplication),
+# / (division),
+# w (printing the n-th primitive root and all its powers).
+# in case of command +, -, *, / parse two complex numbers after the command. In case of the command w, parse an integer n. The execution of the program should stop when entering a blank line.
+# The results should be rounded to 5 decimal places.
