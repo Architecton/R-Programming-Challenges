cd4be739421ea75992de6f9e129400940a243be7
Jernej Vivod <je.vivod@gmail.com>
2018-09-13 15:32:21 +0200

    Add instructions for third challenge

---
+++ Challenge_3/challenge_3.R
@@ -0,0 +1,6 @@
+# Write a program that computes the DFT of a given vector of polynomial coefficients using a recursive FFT algorithm.
+# Print out a trace after every merge. Print the coefficients of the merged polynomial.
+
+# input:
+#	- The first argument to the program is the length of the vector (the number of polynomial coefficients)
+#	- The second argument to the program
